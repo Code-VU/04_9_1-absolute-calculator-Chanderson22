9b19e8f1de0a5a918d3ec631483d090dc53db45d
Chad Anderson <Chad.Anderson@vu.com>
2025-02-04 16:05:04 -0600

    convert n to int, set up if else to do different calc based on value, return absolute and print result

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -2,6 +2,11 @@ def calculateAbsolute():
     
     # This first line is provided for you
     in_num  = input("Enter a number: ")
+    n = int(in_num)
+    if n > 21:
+        print("Result: "+str(abs(2*(n-21))))
+    elif n < 21:
+        print("Result: "+str(abs(n-21)))
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
